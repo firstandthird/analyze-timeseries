09c4fac7a2727c0e3e81d6090ebaae179e76a9d6
Antonio Laguna <antonio@potatolondon.com>
2015-04-30 17:12:58 +0200

    First pass to moment timezone
    Not sure if clever or hacky

--- index.js
+++ index.js
@@ -1,34 +1,46 @@
+var momentNoTz = require('moment');
+var momentTz = require('moment-timezone');
 var formatTimeseries = require('./lib/timeseries');
 var getMetrics = require('./lib/metrics');
 
 module.exports = function(data, options) {
+  var moment;
   options = options || {};
-  var timeseries = formatTimeseries(data, options);
+
+  if (options.timezone) {
+    moment = function() {
+      return momentTz.apply(this, arguments).tz(options.timezone);
+    };
+  } else {
+    moment = momentNoTz;
+  }
+
+  var timeseries = formatTimeseries(data, options, moment);
 
   //metrics
   var metrics = {
     daily: {
-      today: getMetrics.today(timeseries),
-      latest: getMetrics.latest(timeseries),
-      daysAgo7: getMetrics.daysAgo(timeseries, 7),
-      daysAgo14: getMetrics.daysAgo(timeseries, 14),
-      daysAgo21: getMetrics.daysAgo(timeseries, 21)
+      today: getMetrics.today(timeseries, moment),
+      latest: getMetrics.latest(timeseries, moment),
+      daysAgo7: getMetrics.daysAgo(timeseries, 7, moment),
+      daysAgo14: getMetrics.daysAgo(timeseries, 14, moment),
+      daysAgo21: getMetrics.daysAgo(timeseries, 21, moment)
     },
     weekly: null,
     overall: {
-      maximum: getMetrics.max(timeseries),
-      minimum: getMetrics.min(timeseries)
+      maximum: getMetrics.max(timeseries, moment),
+      minimum: getMetrics.min(timeseries, moment)
     }
   };
 
   if (options.aggregates !== false && !options.ranking) {
-    metrics.overall.total = getMetrics.total(timeseries);
-    metrics.overall.average = getMetrics.average(timeseries);
+    metrics.overall.total = getMetrics.total(timeseries, moment);
+    metrics.overall.average = getMetrics.average(timeseries, moment);
     metrics.weekly = {
-      thisWeek: getMetrics.weekAgo(timeseries, 0),
-      weekAgo1: getMetrics.weekAgo(timeseries, 1),
-      weekAgo2: getMetrics.weekAgo(timeseries, 2),
-      weekAgo3: getMetrics.weekAgo(timeseries, 3)
+      thisWeek: getMetrics.weekAgo(timeseries, 0, moment),
+      weekAgo1: getMetrics.weekAgo(timeseries, 1, moment),
+      weekAgo2: getMetrics.weekAgo(timeseries, 2, moment),
+      weekAgo3: getMetrics.weekAgo(timeseries, 3, moment)
     };
   }
 
